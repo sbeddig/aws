1e28f922ba0b94e307d5b88fa38b1b1a3f57f780
Simon Beddig <simon.beddig@googlemail.com>
2020-10-26 13:00:40 +0100

    update

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkStack } from '../lib/cdk-stack';
+import {CdkStack} from '../lib/cdk-stack';
 
 const app = new cdk.App();
-const defaultEnv = { account: '675159688907', region: 'eu-west-1' };
+const defaultEnv = {account: '675159688907', region: 'eu-west-1'};
 
 new CdkStack(app, 'CdkStack', {
     env: defaultEnv
