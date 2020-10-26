f5ca54a126014b92c0e7220535ca8b2d9e9e8343
Simon Beddig <simon.beddig@googlemail.com>
2020-10-26 12:59:47 +0100

    update

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,4 +4,8 @@ import * as cdk from '@aws-cdk/core';
 import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+const defaultEnv = { account: '675159688907', region: 'eu-west-1' };
+
+new CdkStack(app, 'CdkStack', {
+    env: defaultEnv
+});
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as sns from '@aws-cdk/aws-sns';
 import * as sqs from '@aws-cdk/aws-sqs';
-import { SubscriptionProtocol } from '@aws-cdk/aws-sns';
+import * as subscriptions from '@aws-cdk/aws-sns-subscriptions'
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -15,10 +15,7 @@ export class CdkStack extends cdk.Stack {
       queueName: "testQueue",
     })
 
-    const subscription = new sns.Subscription(this, "testSubscription", {
-      topic: topic,
-      endpoint: queue.queueArn,
-      protocol: SubscriptionProtocol.SQS
-    })
+    topic.addSubscription(new subscriptions.SqsSubscription(queue))
+
   }
 }
