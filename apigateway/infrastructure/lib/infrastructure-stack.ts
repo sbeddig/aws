26f4f41778e566a233b8ea651152155f5ea4db3c
Simon Beddig <simon.beddig@googlemail.com>
2021-01-17 13:41:48 +0100

    wip

--- apigateway/infrastructure/lib/infrastructure-stack.ts
+++ apigateway/infrastructure/lib/infrastructure-stack.ts
@@ -3,6 +3,8 @@ import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 import * as api from '@aws-cdk/aws-apigateway';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as path from 'path';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -16,8 +18,17 @@ export class InfrastructureStack extends cdk.Stack {
 
     topic.addSubscription(new subs.SqsSubscription(queue));
 
-    const helloWorldApi = new api.SpecRestApi(this, 'hello-world-api', {
-      apiDefinition: api.ApiDefinition.fromAsset('../api/api.yaml')
+    new lambda.Function(this, 'MyGoFunction', {
+      runtime: lambda.Runtime.GO_1_X,
+      handler: 'main',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../build/getAll.zip')),
     });
+
+    // const helloWorldApi = new api.SpecRestApi(this, 'hello-world-api', {
+    //   apiDefinition: api.ApiDefinition.fromAsset('../api/api.yaml')
+    // });
+
+
+
   }
 }
